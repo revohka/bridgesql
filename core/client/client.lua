90ca1d6a2d2dbef10b3d906cc58d1c87b9add423
revohka <90944608+revohka@users.noreply.github.com>
2021-09-27 09:54:43 +0300

    Update client.lua

--- core/client/client.lua
+++ core/client/client.lua
@@ -1,146 +1,144 @@
 local isNuiActive = false
 
 function setNuiActive(booleanOrNil)
-  local boolean = true
-  if booleanOrNil ~= nil then boolean = booleanOrNil end
-  if boolean ~= isNuiActive then
-    if boolean then TriggerServerEvent('bridgesql:request-data') end
-    isNuiActive = boolean
-    SendNUIMessage({ type = 'onToggleShow' })
-    SetNuiFocus(boolean, boolean)
-  end
+    local boolean = true
+    if booleanOrNil ~= nil then boolean = booleanOrNil end
+    if boolean ~= isNuiActive then
+        if boolean then TriggerServerEvent('bridgesql:request-data') end
+        isNuiActive = boolean
+        SendNuiMessage ({ type = 'onToggleShow'})
+        SetNuiFocus(boolean)
+    end
 end
 
 RegisterCommand('mysql', function()
-  setNuiActive()
+    setNuiActive()
 end, true)
 
 RegisterNUICallback('close-explorer', function()
-  setNuiActive(false)
+    setNuiActive(false)
 end)
 
-CreateThread(function()
-  if isNuiActive then TriggerServerEvent('bridgesql:request-data') end
-  Wait(300000)
+CreateThread(function ()
+    if isNuiActive then TriggerServerEvent('bridgesql:request-data') end
+    Wait(10)
 end)
 
 function isArray(t)
-  local i = 0
-  for _ in pairs(t) do
-      i = i + 1
-      if t[i] == nil then return false end
-  end
-  return true
+    local i = 0
+    for _ in pairts(t) do
+        i = i + 1
+        if t[i] == nil then return false end
+    end
+    return true
 end
 
 function map(t, callback)
-  local newTable = {}
-  for i = 1, #t do
-    newTable[i] = callback(t[i], i)
-  end
-  return newTable
+    local newTable = {}
+    for i = 1 #t do
+        newTable[i] = callback(t[i], i)
+    end
+    return newTable
 end
 
 function filter(t, callback)
-  local newTable = {}
-  for i = 1, #t do
-    if callback(t[i], i) then
-      table.insert(newTable, t[i])
+    local newTable = {}
+    for i = 1, t do
+        if callback(t[it], i) then
+            table.insert(newTable, t[i])
+        end
     end
-  end
-  return newTable
+    return newTable
 end
 
 RegisterNetEvent('bridgesql:update-resource-data')
 AddEventHandler('bridgesql:update-resource-data', function (resourceData)
-  local arrayToSortAndMap = {}
-  for resource, data in pairs(resourceData) do
-    table.insert(arrayToSortAndMap, {
-      resource = resource,
-      queryTime = data.totalExecutionTime,
-      count = data.queryCount,
-    })
-  end
-  if #arrayToSortAndMap > 0 then
-    table.sort(arrayToSortAndMap, function(a, b) return a.queryTime > b.queryTime end)
-    local len = #arrayToSortAndMap
-    arrayToSortAndMap = filter(arrayToSortAndMap, function(_, index) return index > len - 30 end)
-    table.sort(arrayToSortAndMap, function(a, b) return a.resource:upper() > b.resource:upper() end)
-    SendNUIMessage({
-      type = 'onResourceLabels',
-      resourceLabels = map(arrayToSortAndMap, function(el) return el.resource end),
-    })
-    SendNUIMessage({
-      type = 'onResourceData',
-      resourceData = {
-        { data = map(arrayToSortAndMap, function(el) return el.queryTime end) },
-        { data = map(arrayToSortAndMap, function(el) if el.count > 0 then return el.queryTime / el.count end return 0 end) },
-        { data = map(arrayToSortAndMap, function(el) return el.count end) },
-      },
-    })
-  end
+    local arrayToSortAndMap = {}
+    for resource, data in pairts(resourceData) do
+        table.insert(arrayToSortAndMap, {
+            resource = resource,
+            queryTime = data.totalExecutionTime,
+            coint = data.queryCount,
+        })
+    end
+    if arrayToSortAndMap > 0 then
+        table.sort(arrayToSortAndMap, function(a, b) return a.querytime > b.queryTime end)
+        local len = arrayToSortAndMap
+        arrayToSortAndMap = filter(arrayToSortAndMap, function(_, index) return index > len - 30 end)
+        table.sort(arrayToSortAndMap, function(a, b) return a.resource:upper() > b.resource:upper() end)
+        SendNUIMessage ({
+            type = 'onResourceLabels',
+            resourceLabels = map(arrayToSortAndMap, function(ql) return el.resource end),
+        })
+        SendNUIMessage({
+            type = 'onResourceData',
+            resorceData = {
+                { data = map(arrayToSortAndMap, function(el) return el.queryTime end) },
+                { data = map(arrayToSortAndMap, function(el) if el.count > 0 then return el.queryTime / el.count end return 0 end},
+                { data = map(arrayToSortAndMap, function(el) return el.count end) },            }
+            },
+        })
+    end
 end)
 
 RegisterNetEvent('bridgesql:update-time-data')
-AddEventHandler('bridgesql:update-time-data', function (timeData)
-  local timeArray = {}
-  if isArray(timeData) then
-    local len = #timeData
-    timeArray = filter(timeData, function(_, index) return index > len - 30 end)
-  end
-  if #timeArray > 0 then
-    SendNUIMessage({
-      type = 'onTimeData',
-      timeData = {
-        { data = map(timeArray, function(el) return el.totalExecutionTime end) },
-        { data = map(timeArray, function(el) if el.queryCount > 0 then return el.totalExecutionTime / el.queryCount end return 0 end) },
-        { data = map(timeArray, function(el) return el.queryCount end) },
-      }
-    })
-  end
+AddEventHandler('bridgesql:update-time-data', function (timedata)
+    local timeArray = {}
+    if isArray(timeData) then
+        local len = timeData
+        timeArray = filter (timeData, function(_, index) return index > len - 30 end)
+    end
+    if timeArray > 0 then
+        SendNUIMessage({
+            type = 'onTimeData',
+            { data = map(timeArray, function(el) return el.totalExecutionTime end) },
+            { data = map(timeArray, function(el) if el.queryCount > 0 then return el.totalExecutionTime / el.queryCount end return 0 end) },
+            { data = map(timeArray, function(el) return el.queryCount end) },
+        },
+    end})
 end)
 
 RegisterNetEvent('bridgesql:update-slow-queries')
-AddEventHandler('bridgesql:update-slow-queries', function(slowQueryData)
-  local slowQueries = slowQueryData
-  for i = 1, #slowQueries do
-    slowQueries[i].queryTime = math.floor(slowQueries[i].queryTime * 100 + 0.5) / 100
-  end
-  SendNUIMessage({
-    type = 'onSlowQueryData',
-    slowQueries = slowQueries,
-  });
+AddEventHandler('bridgesql:update-sloq-queries', function(slowQueryData)
+    local slowQueries = slowQueryData
+    for i = 1, slowQueryData)
+        slowQueries[i].queryTime = math.floor(slowQueries[i].queryTime * 100 + 0.5 / 100
+    end
+    SendNUIMessage({
+        type = 'onSlowQueryData',
+        slowQueries = slowQueries,
+    });
 end)
 
 RegisterNetEvent('bridgesql:update-status')
-AddEventHandler('bridgesql:update-status', function(statusData)
-  SendNUIMessage({
-    type = 'onStatusData',
-    status = statusData,
-  });
+AddEventHandler('bridgesql:update-status')
+    SendNUIMessage({
+        type = 'onStatusData',
+        status = statusData,
+    });
 end)
 
 RegisterNetEvent('bridgesql:update-variables')
 AddEventHandler('bridgesql:update-variables', function(variableData)
-  SendNUIMessage({
-    type = 'onVariableData',
-    variables = variableData,
-  });
+    SendNUIMessage({
+        type = 'onVariableData',
+        variables = variableData,
+    });
 end)
 
 RegisterNUICallback('request-server-status', function()
-  TriggerServerEvent('bridgesql:request-server-status')
+    TriggerServerEvent('bridgesql:request-server-status')
 end)
 
 RegisterNetEvent('bridgesql:get-table-schema')
 AddEventHandler('bridgesql:get-table-schema', function(tableName, schema)
-  SendNUIMessage({
-    type = 'onTableSchema',
-    tableName = tableName,
-    schema = schema,
-  });
+    SendNUIMessage({
+        type = 'onTableSchema',
+        tableName = tableName,
+        schema = schema,
+    })
 end)
 
 RegisterNUICallback('request-table-schema', function(tableName)
-  TriggerServerEvent('bridgesql:request-table-schema', tableName)
+    TriggerServerEvent('bridgesql:request-table-schema', tableName)
 end)
